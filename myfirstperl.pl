38c0cd5798d9adb6163e9cd8b2a55a4b57829b70
Marlo <marlokmellon@gmail.com>
2020-03-03 15:12:19 +0000

    my first perl

---
+++ myfirstperl.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+$DNA='ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+$DNA2='ATAGTGCCGTGAGAGTGATGATGAT';
+
+print "Here are the original two DNA fragments:\n\n";
+print $DNA,"\n";
+print $DNA2,"\n\n";
+
+$DNA3="$DNA$DNA2";
+print "Here is the concatenation of the first two gragments (version 1):\t";
+print $DNA,"\n\n";
+
+$DNA='ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+print "Here is the starting DNA:\n\n";
+print "$DNA\n\n";
+$RNA=$DNA;
+$RNA=~s/T/U/g;
+print "Here is the result of transcribing the DNA to RNA:\n\n";
+print "$RNA\n";
